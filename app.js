730a2b29094566406b2b077f332bde13d566f844
Juan Castro <juancastro@firstfactory.com>
2019-08-30 14:33:08 -0600

    added a filter function

--- app.js
+++ app.js
@@ -16,8 +16,11 @@ function loadEventListeners() {
     // remove task event
     taskList.addEventListener('click', removeTask);
 
-    // clear task events
+    // clear tasks event
     clearBtn.addEventListener('click', clearTasks);
+
+    // filter tasks event
+    filter.addEventListener('keyup', filterTasks);
 }
 
 // add task
@@ -74,4 +77,20 @@ function clearTasks () {
             taskList.removeChild(taskList.firstChild);
         }
     }
+}
+
+// filter tasks
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('collection-item').forEach(
+        function(task){
+            const item = task.firstChild.textContent;
+            if (item.toLowerCase().indexOf(text) != -1) {
+                task.style.display = 'block';
+            } else {
+                task.style.display = 'none';
+            }
+        }
+    );
 }
